26603d4afb6c529eaa178542e815bfaa174e2108
christianmader <christian.mader@univie.ac.at>
2016-11-17 16:43:53 +0100

    initial commit

--- src/main/java/de/fhg/ids/app/datadump/Controller.java
+++ src/main/java/de/fhg/ids/app/datadump/Controller.java
@@ -1,6 +1,5 @@
 package de.fhg.ids.app.datadump;
 
-import de.fhg.ids.app.datadump.domainobj.Dataset;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryResult;
@@ -9,14 +8,16 @@ import org.eclipse.rdf4j.rio.turtle.TurtleWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.AbstractResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
 import java.util.Collection;
-import java.util.Optional;
 
 @RestController
 public class Controller  {
@@ -24,49 +25,22 @@ public class Controller  {
     private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     @Autowired
-    DatasetService service;
+    NodeService service;
 
-    @RequestMapping(value = "/meta", method = RequestMethod.GET)
-    public void getAllMetadata(HttpServletResponse response) throws IOException {
-        RepositoryConnection repCon = service.getRepository().getConnection();
-        RDFWriter writer = new TurtleWriter(response.getOutputStream());
-        writer.startRDF();
+    @RequestMapping(value = "/nodes/{url}", method = RequestMethod.POST)
+    public void addNode(HttpServletRequest request, @PathVariable URL url) throws IOException {
+        BufferedReader omiReadRequestReader = request.getReader();
 
-        RepositoryResult<Statement> allStatements = repCon.getStatements(null, null, null);
-        while (allStatements.hasNext()) {
-            writer.handleStatement(allStatements.next());
-        }
-        writer.endRDF();
-        response.getOutputStream().close();
+        // todo:
+        // 1. add node to list of "managed" nodes
+        // 2. subscribe at the node for the objects passed in the request body
+
+        addNode();
     }
 
-    @RequestMapping(value = "/", produces = "application/json")
-    @ResponseBody
-    public Collection<Dataset> getDatasets() {
-        return service.getDatasets();
-    }
-
-    @RequestMapping(value = "/dataset/{datasetId}", method = RequestMethod.GET, produces = "application/x-turtle")
-    @ResponseBody
-    public AbstractResource getDataset(@PathVariable long datasetId) {
-        Optional<Dataset> datasetDescription = service.getDataset(datasetId);
-        if (datasetDescription.isPresent()) {
-            Dataset dataset = datasetDescription.get();
-            return new FileSystemResource(dataset.getFilename());
-        }
-
-        throw new IllegalArgumentException("no such dataset id");
-    }
-
-    @RequestMapping(value = "/dataset/{datasetId}/meta", method = RequestMethod.GET)
-    public AbstractResource getMetadata(HttpServletResponse response, @PathVariable long datasetId) throws IOException {
-        Optional<Dataset> datasetDescription = service.getDataset(datasetId);
-        if (datasetDescription.isPresent()) {
-            Dataset dataset = datasetDescription.get();
-            return new FileSystemResource(dataset.getMetadataFilename());
-        }
-
-        throw new IllegalArgumentException("no such dataset id");
+    @RequestMapping(value = "/nodes", method = RequestMethod.GET)
+    public Collection<Node> getNodes() {
+        return service.getNodes();
     }
 
 }
---
+++ src/main/java/de/fhg/ids/app/datadump/Node.java
@@ -0,0 +1,13 @@
+package de.fhg.ids.app.datadump;
+
+import java.net.URL;
+
+/**
+ * Created by christian on 17.11.16.
+ */
+public class Node {
+
+    private URL url;
+    private String subscriptionXMLRequest;
+
+}
