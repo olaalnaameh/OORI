1daad200e42d86da184d4de0cdc3335a0e59dec2
christianmader <christian.mader@univie.ac.at>
2016-11-18 11:20:53 +0100

    added code that issues http post of subscription request

--- src/main/java/de/fhg/ids/app/datadump/NodeService.java
+++ src/main/java/de/fhg/ids/app/datadump/NodeService.java
@@ -1,6 +1,14 @@
 package de.fhg.ids.app.datadump;
 
 import de.fhg.ids.app.datadump.exceptions.OMIRequestCreationException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,6 +26,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.*;
 
@@ -44,7 +53,14 @@ class NodeService {
     public void subscribe(OmiNode omiNode) {
         logger.debug("subscribing omiNode " + omiNode.toString());
         String subscriptionRequest = createSubscriptionRequest(omiNode.getSubscriptionXMLRequest());
-        sendRequest(omiNode.getUrl(), subscriptionRequest);
+
+        try {
+            sendRequest(omiNode.getUrl(), subscriptionRequest);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String createSubscriptionRequest(String subscriptionXMLRequest) {
@@ -87,8 +103,28 @@ class NodeService {
         return writer.toString();
     }
 
-    private void sendRequest(URL url, String request) {
+    private void sendRequest(URL url, String request) throws URISyntaxException, IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url.toURI());
 
+        post.setEntity(new StringEntity(request));
+
+        HttpResponse response = client.execute(post);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + post.getEntity());
+        System.out.println("Response Code : " +
+                response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result.toString());
     }
 
     public void unsubscribe(OmiNode omiNode) {
---
+++ src/test/de/fhg/ids/app/datadump/NodeServiceTest.java
@@ -0,0 +1,10 @@
+package de.fhg.ids.app.datadump;
+
+/**
+ * Created by christian on 18.11.16.
+ */
+public class NodeServiceTest {
+
+
+
+}
