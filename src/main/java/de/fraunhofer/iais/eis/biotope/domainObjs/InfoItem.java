34695edafb37187f8d1c70812089c3af6743e92d
christianmader <christian.mader@univie.ac.at>
2016-11-23 19:24:07 +0100

    added serialization of metadata

--- src/main/java/de/fraunhofer/iais/eis/biotope/domainObjs/InfoItem.java
+++ src/main/java/de/fraunhofer/iais/eis/biotope/domainObjs/InfoItem.java
@@ -11,6 +11,7 @@ import org.eclipse.rdf4j.model.vocabulary.RDF;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -18,7 +19,8 @@ import java.util.HashSet;
 public class InfoItem {
 
     private String name;
-    private Collection<Value> values;
+    private Collection<Value> values = new ArrayList<>();
+    private Collection<MetaData> metaData = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -36,6 +38,15 @@ public class InfoItem {
     @XmlElement(name = "value")
     public void setValues(Collection<Value> values) {
         this.values = values;
+    }
+
+    public Collection<MetaData> getMetaData() {
+        return metaData;
+    }
+
+    @XmlElement(name = "MetaData")
+    public void setMetaData(Collection<MetaData> metaData) {
+        this.metaData = metaData;
     }
 
     public Model serialize(ValueFactory vf, String infoItemBaseIri) {
@@ -58,8 +69,17 @@ public class InfoItem {
             builder.add("odf:value", valueId);
         });
 
+        Collection<Model> metaDataModels = new HashSet<>();
+        metaData.forEach(md -> metaDataModels.add(md.serialize(vf, subject + "/")));
+
+        metaDataModels.forEach(model -> {
+            Resource metadataId = model.iterator().next().getSubject();
+            builder.add("odf:metadata", metadataId);
+        });
+
         Model infoItemModel = builder.build();
         valueModels.forEach(valueModel -> infoItemModel.addAll(valueModel));
+        metaDataModels.forEach(metadataModel -> infoItemModel.addAll(metadataModel));
 
         return infoItemModel;
     }
---
+++ src/main/java/de/fraunhofer/iais/eis/biotope/domainObjs/MetaData.java
@@ -0,0 +1,53 @@
+package de.fraunhofer.iais.eis.biotope.domainObjs;
+
+import de.fraunhofer.iais.eis.biotope.vocabs.NS;
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Collection;
+import java.util.HashSet;
+
+@XmlRootElement
+public class MetaData {
+
+    private Collection<InfoItem> infoItems;
+
+    public Collection<InfoItem> getInfoItems() {
+        return infoItems;
+    }
+
+    @XmlElement(name = "InfoItem")
+    public void setInfoItems(Collection<InfoItem> infoItems) {
+        this.infoItems = infoItems;
+    }
+
+    public Model serialize(ValueFactory vf, String baseIri) {
+        BNode subject = vf.createBNode();
+
+        ModelBuilder builder = new ModelBuilder();
+        builder.setNamespace("dct", NS.DCT)
+                .setNamespace("odf", NS.ODF)
+                .setNamespace("rdf", RDF.NAMESPACE)
+
+                .subject(subject)
+                .add("rdf:type", "odf:MetaData");
+
+        Collection<Model> infoItemModels = new HashSet<>();
+        infoItems.forEach(infoitem -> infoItemModels.add(infoitem.serialize(vf, baseIri)));
+
+        infoItemModels.forEach(model -> {
+            builder.add("odf:infoitem", model.iterator().next().getSubject());
+        });
+
+        Model metadataModel = builder.build();
+        infoItemModels.forEach(infoItemModel -> metadataModel.addAll(infoItemModel));
+
+        return metadataModel;
+    }
+
+}
