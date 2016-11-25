fe0fd5ccf44f75fc2e7972d555bcc11de3519d79
christianmader <christian.mader@univie.ac.at>
2016-11-25 17:47:04 +0100

    writing to a sparql repository works

--- src/main/java/de/fraunhofer/iais/eis/biotope/OdfRdfRepository.java
+++ src/main/java/de/fraunhofer/iais/eis/biotope/OdfRdfRepository.java
@@ -3,8 +3,8 @@ package de.fraunhofer.iais.eis.biotope;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.http.HTTPRepository;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,20 +18,20 @@ public class OdfRdfRepository {
 
     private final Logger logger = LoggerFactory.getLogger(OdfRdfRepository.class);
 
-    @Value("${repository.url:}")
-    private String repositoryUrl;
+    @Value("${endpoint.url:}")
+    private String endpointUrl;
 
     private Repository repository;
 
     @PostConstruct
     private void postConstruct() {
-        if (repositoryUrl == null || repositoryUrl.isEmpty()) {
+        if (endpointUrl == null || endpointUrl.isEmpty()) {
             logger.info("Preparing memory repository");
             repository = new SailRepository(new MemoryStore());
         }
         else {
-            logger.info("Setting HTTP repository at '" +repositoryUrl+ "'");
-            repository = new HTTPRepository(repositoryUrl);
+            logger.info("Setting HTTP repository at '" + endpointUrl + "'");
+            repository = new SPARQLRepository(endpointUrl);
         }
 
         repository.initialize();
