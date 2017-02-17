727957e7a99bedcb26a5e85d114d9f5bd566c48c
christianmader <christian.mader@univie.ac.at>
2017-02-17 17:10:43 +0100

    added debug code

--- src/main/java/de/fraunhofer/iais/eis/biotope/Controller.java
+++ src/main/java/de/fraunhofer/iais/eis/biotope/Controller.java
@@ -1,5 +1,6 @@
 package de.fraunhofer.iais.eis.biotope;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
 import java.util.Collection;
 
 @RestController
@@ -33,10 +36,12 @@ public class Controller  {
     }
 
     @RequestMapping(value = "/callback", method = RequestMethod.POST)
-    public void callback(HttpServletRequest body) {
+    public void callback(HttpServletRequest body) throws IOException {
         String messageContent = body.getParameter("msg");
         if (messageContent == null) {
             logger.info("Received callback message with no content");
+
+            System.out.println(IOUtils.toString(body.getInputStream(), Charset.defaultCharset()));
         }
         else {
             service.persistOmiMessageContent(body.getParameter("msg"));
